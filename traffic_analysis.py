09b75c4f76141ce353f6df432b7e1cb9d674c710
chandramouli-sastry <chandramouli.sastry@gmail.com>
2016-08-15 22:15:01 +0530

    m

--- traffic_analysis.py
+++ traffic_analysis.py
@@ -29,6 +29,9 @@ def retreiveData(path,keys=["TIMESTAMP","vehicleCount"]):
             for key in keys:
                 d.__dict__[key]=row[key]
     pass
+max_report_id=None
+max_count = None
+for report_id in data_store:
+    p=filter(data_store[report_id],lambda x:<=Date(x.TIMESTAMP)<=)
+    sum([i.vehileCount for i in p])
 
-
-#filter(data_store[report_id],lambda x:<=x.TIMESTAMP<=)
