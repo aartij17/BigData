b735d03eab8eab503a5aeb65ab6b7f80000aed64
Gautam B <t-gauba@microsoft.com>
2016-08-15 23:01:45 +0530

    Implemented retrieve_meta_data method.

--- traffic_analysis.py
+++ traffic_analysis.py
@@ -1,37 +1,44 @@
-import csv,os
+import csv, os
 
-data_store={}#{report_id: [Data objects]}
-metadata={}#{report_id: {key: value}}
+data_store = {}  # {report_id: [Data objects]}
+metadata = {}  # {report_id: {key: value}}
 
-class Data(Object):
+
+class Data:
     def __init__(self):
         pass
 
 
-def retreiveMetaData(path,keys=["POINT_1_STREET","POINT_2_STREET"],key="REPORT_ID"):
-    ''' Retrieves the keys for each report ID mentioned in meta_data file(path)
-        and populates metadata
-    '''
-    
-    pass
+def retrieve_meta_data(path, keys=("POINT_1_STREET", "POINT_2_STREET"), key="REPORT_ID"):
+    """ Retrieves the keys for each report ID mentioned in meta_data file(path)
+        and populates metadata.
+        :param key: Value to act as key in meta-data.
+        :param keys: Keys to extract
+        :param path: Path to meta-data csv file
+    """
+    with open(path, 'rb') as metadata_file:
+        reader = csv.DictReader(metadata_file)
+        for row in reader:
+            metadata[row[key]] = {_key: row[_key] for _key in keys}
 
 
-def retreiveData(path,keys=["TIMESTAMP","vehicleCount"]):
+def retreiveData(path, keys=["TIMESTAMP", "vehicleCount"]):
     ''' Retrieves data and populates data_store'''
 
     files = os.listdir(path)
     for file in files:
-        f=open(file)
+        f = open(file)
         csv.DictReader(f)
- #       ....
+        #       ....
         for row in f:
-            d=Data()
+            d = Data()
             for key in keys:
-                d.__dict__[key]=row[key]
+                d.__dict__[key] = row[key]
     pass
-max_report_id=None
-max_count = None
-for report_id in data_store:
-    p=filter(data_store[report_id],lambda x:<=Date(x.TIMESTAMP)<=)
-    sum([i.vehileCount for i in p])
 
+
+# max_report_id = None
+# max_count = None
+# for report_id in data_store:
+#     p = filter(data_store[report_id], lambda x: <= Date(x.TIMESTAMP) <=)
+#     sum([i.vehileCount for i in p])
